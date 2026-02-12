9049e66ca427933bd30db9489218ac11b0e40ac1
muin-15 <140100907+muin-15@users.noreply.github.com>
2026-02-13 03:36:22 +0530

    added character and it's movements like walk,idle and jump with other characters like slimes

---
+++ scenes/blue_wizard.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	
+	if not is_on_floor():
+		velocity.y+=980*delta
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# 3. Get direction (Works with Arrows OR A/D now)
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if not is_on_floor():
+		$AnimatedSprite2D.play("jump")
+	elif velocity.x!=0:
+		$AnimatedSprite2D.play("walk")
+		if velocity.x < 0:
+			$AnimatedSprite2D.flip_h = true #left
+		else:
+			$AnimatedSprite2D.flip_h = false #right
+	else:
+		$AnimatedSprite2D.play("idle")
+	move_and_slide()
