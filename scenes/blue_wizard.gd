64371c07aac47102111b014bc4569d30758f29f0
muin-15 <140100907+muin-15@users.noreply.github.com>
2026-02-14 14:00:22 +0530

    added sound effects to the main character blue wizard.Also added different plants animations

--- scenes/blue_wizard.gd
+++ scenes/blue_wizard.gd
@@ -2,16 +2,19 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@onready var sounds={
+	"walking":$AnimatedSprite2D/walk,
+	"jumping":$AnimatedSprite2D/jump,
+}
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	
 	if not is_on_floor():
-		velocity.y+=980*delta
+		velocity.y+=gravity*delta
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		sounds["jumping"].play()
 	# 3. Get direction (Works with Arrows OR A/D now)
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
@@ -21,12 +24,16 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if not is_on_floor():
 		$AnimatedSprite2D.play("jump")
+		sounds["walking"].stop()
 	elif velocity.x!=0:
 		$AnimatedSprite2D.play("walk")
 		if velocity.x < 0:
 			$AnimatedSprite2D.flip_h = true #left
 		else:
 			$AnimatedSprite2D.flip_h = false #right
+		if not sounds["walking"].playing:
+			sounds["walking"].play()
 	else:
 		$AnimatedSprite2D.play("idle")
+		sounds["walking"].stop()
 	move_and_slide()
