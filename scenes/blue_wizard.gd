db55d4438a304a712cd45d9c04eeaa4a0f78ca38
muin-15 <140100907+muin-15@users.noreply.github.com>
2026-02-18 02:26:41 +0530

    Everthing is playable,no glitches no errors

--- scenes/blue_wizard.gd
+++ scenes/blue_wizard.gd
@@ -3,18 +3,33 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const DOUBLE_VELOCITY = -800.0
-var double_is_used=false
+var health = 3
+var max_health = 3
+var is_invincible = false
+
 @onready var sounds={
 	"walking":$AnimatedSprite2D/walk,
 	"jumping":$AnimatedSprite2D/jump,
+	"dashing":$AnimatedSprite2D/dash
 }
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	
 	if not is_on_floor():
-		velocity.y+=gravity*delta
+		velocity.y += gravity * delta
+
+	# -----------------------------------------------
+	# PASTE THIS CODE HERE:
+	# If we are hurt, apply movement but STOP the rest of the function
+	# so we don't overwrite the 'dash1' animation.
+	if is_invincible:
+		move_and_slide()
+		return
+	# -----------------------------------------------
+
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		# ... rest of your code
 		velocity.y = JUMP_VELOCITY
 		sounds["jumping"].play()
 	if Input.is_action_just_pressed("double_jump") and is_on_floor():
@@ -45,3 +60,45 @@ func _physics_process(delta):
 		sounds["walking"].stop()
 	move_and_slide()
 	print(velocity.x)
+
+func take_damage():
+	# If the player is already invincible (because they just got hit),
+	# do nothing. Stop the function right here.
+	if is_invincible:
+		return
+	
+	# If we are NOT invincible, then...
+	is_invincible = true # 1. Turn on invincibility.
+	health -= 1          # 2. Subtract 1 from our health.
+	print("Player health is now: ", health) # (Optional) Shows health in the Output window for debugging.
+	if not sounds["walking"] or sounds ["jumping"]:
+		sounds["dashing"].play()
+	# 3. Tell the animation sprite to play the damage animation.
+	$AnimatedSprite2D.play("dash1") 
+	
+	# 4. Check if the game is over.
+	if health <= 0:
+		print("GAME OVER")
+		# For now, just reload the scene to restart.
+		get_tree().reload_current_scene()
+		
+# --- ADD THIS EMPTY FUNCTION FOR NOW ---
+# This function will be connected to a signal from the animation player.
+# It will run automatically when ANY animation finishes.
+func _on_animated_sprite_2d_animation_finished():
+	# We only care if the "dash" animation was the one that finished.
+	if $AnimatedSprite2D.animation == "dash1":
+		# If it was, go back to the idle animation.
+		$AnimatedSprite2D.play("idle")
+		# And, most importantly, turn invincibility OFF so we can get hurt again.
+		is_invincible = false
+# -----------------------------------------
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		print("GOAL REACHED! Level Complete.")
+		
+		# *** This is the win action ***
+		# Simply reload the scene, just like the player does on death.
+		get_tree().reload_current_scene()# Replace with function body.
--- scenes/plant.gd
+++ scenes/plant.gd
@@ -14,6 +14,18 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # We make it negative because in Godot, Up is Negative.
 var jump_force = -sqrt(2 * gravity * target_height)
 
+func _on_area_2d_body_entered(body):
+	# 1. Check if the body that entered is the player.
+	# We do this by checking if it belongs to a group named "player".
+	# This is better than checking body.name == "Player" because it's more flexible.
+	if body.is_in_group("player"):
+		# 2. Check if the player object actually has a function called "take_damage".
+		# This is a safety check to prevent sthe game from crashing if we hit something else.
+		if body.has_method("take_damage"):
+			
+			# 3. If it's the player and they have the function, call it.
+			# This is like sending a message: "Hey player, you've been hit!"
+			body.take_damage()
 func _physics_process(delta):
 	# 1. Apply Gravity (Fall down)
 	if not is_on_floor():
@@ -28,3 +40,7 @@ func _physics_process(delta):
 		# $AnimatedSprite2D.play("jump")
 
 	move_and_slide()
+
+
+# This function runs automatically whenever a solid body (like the player)
+# enters the Area2D we just made. The 'body' variable is a reference to that object.
